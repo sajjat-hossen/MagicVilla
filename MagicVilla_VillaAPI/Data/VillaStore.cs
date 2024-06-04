bb39b714db9d52273ccf5edf8053733a1e00cf6e
sajjat-hossen <sajjat.hossen@brainstation-23.com>
2024-06-04 11:35:10 +0600

    update villaStore

--- MagicVilla_VillaAPI/Data/VillaStore.cs
+++ MagicVilla_VillaAPI/Data/VillaStore.cs
@@ -7,7 +7,8 @@ namespace MagicVilla_VillaAPI.Data
         public static List<VillaDTO> villaList = new List<VillaDTO>
             {
                 new VillaDTO { Id = 1, Name = "Pool View", Sqft = 400, Occupancy = 3},
-                new VillaDTO { Id = 2, Name = "Beach View", Sqft = 300, Occupancy = 2}
+                new VillaDTO { Id = 2, Name = "Beach View", Sqft = 300, Occupancy = 2},
+                new VillaDTO { Id = 3, Name = "River View", Sqft = 300, Occupancy = 2}
             };
     }
 }
